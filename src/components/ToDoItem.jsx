1af9589ae57d5238fe5c3be0dc541c04831c166e
izuchrisa <izuchrisa@gmail.com>
2022-04-06 13:37:05 +0100

    to-do-listApp version1

---
+++ src/components/ToDoItem.jsx
@@ -0,0 +1,40 @@
+// ====== This function deletes an item from the array ===============
+import React from "react";
+
+function ToDoItem(props) {
+  return (
+    <div
+      onClick={() => {
+        props.onChecked(props.id);
+      }}
+    >
+      <li>{props.text}</li>
+    </div>
+  );
+}
+
+export default ToDoItem;
+
+// ======= This function crosses the line of the item in the todo list ===========
+
+// import React, { useState } from "react";
+
+// function ToDoItem(props) {
+//   const [isDone, setIsDone] = useState(false);
+
+//   function handleClick() {
+//     setIsDone((prevValue) => {
+//       return !prevValue;
+//     });
+//   }
+
+//   return (
+//     <div onClick={handleClick}>
+//       <li style={{ textDecoration: isDone ? "line-through" : "none" }}>
+//         {props.text}
+//       </li>
+//     </div>
+//   );
+// }
+
+// export default ToDoItem;
